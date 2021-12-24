6461ab617f278cbe860caaab90c2121906aac071
Samshad <samshad.ru@gmail.com>
2021-12-24 17:03:06 +0600

    fixed multiple user issue.

--- postdata.py
+++ postdata.py
@@ -12,29 +12,3 @@ class LevelData(BaseModel):
     substats_count: int
     type: str
 
-
-"""
-{
-    "canlevelup": true,
-    "type": "Goblet of Eonothem",
-    "mainstat": "Hydro DMG Bonus%",
-    "mainstat_value": 7,
-    "substats": [
-        [
-            "CRIT DMG%",
-            6.99
-        ],
-        [
-            "Elemental Mastery",
-            23.31
-        ],
-        [
-            "HP%",
-            4.66
-        ]
-    ],
-    "substats_count": 3,
-    "level": 0,
-    "levelup": 1
-}
-"""
